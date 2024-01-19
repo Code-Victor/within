b991f197b1651fb129a70c3cf9f29fc9c9e0f830
Hamzat Victor <oluwaborihamzat@gmail.com>
2024-01-19 04:10:07 +0100

    feat: add icons

--- src/assets/icons/index.ts
+++ src/assets/icons/index.ts
@@ -2,3 +2,7 @@ export { default as Back } from "./back.svg";
 export { default as Logo } from "./logo.svg";
 export { default as Eye } from "./eye.svg";
 export { default as EyeSlash } from "./eye-slash.svg";
+export { default as Menu } from "./menu.svg";
+export { default as Notification } from "./notification-line.svg";
+export { default as Close } from "./close.svg";
+export { default as ChevronRight } from "./chevron-right.svg";
