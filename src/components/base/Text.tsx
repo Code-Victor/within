ebb30640860e2e58d3fcd3053a17a821d9147f95
Hamzat Victor <oluwaborihamzat@gmail.com>
2024-01-19 01:23:00 +0100

    feat: update text component

--- src/components/base/Text.tsx
+++ src/components/base/Text.tsx
@@ -8,38 +8,47 @@ export const Text = styled(TMText, {
     type: {
       h1: {
         fontSize: "$7.5",
+        lineHeight: "$7.5",
         fontWeight: "700",
       },
       h2: {
         fontSize: "$6",
+        lineHeight: "$6",
         fontWeight: "700",
       },
       h3: {
         fontSize: "$5",
+        lineHeight: "$5",
         fontWeight: "700",
       },
       h4: {
         fontSize: "$4",
+        lineHeight: "$4",
         fontWeight: "700",
       },
       body1: {
         fontWeight: "500",
+        lineHeight: "$3",
         fontSize: "$3",
       },
       body2: {
         fontWeight: "400",
+        lineHeight: "$2",
         fontSize: "$2",
       },
       button1: {
         fontWeight: "500",
+        lineHeight: "$3",
         fontSize: "$3",
       },
       button2: {
         fontWeight: "500",
+        lineHeight: "$2",
         fontSize: "$2",
       },
       caption: {
         fontWeight: "400",
+        lineHeight: "$1",
         fontSize: "$1",
       },
     },
