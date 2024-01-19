57ad4d0c408c2fbc50490de54a88f048b5115ce6
Lensatom <ayomidetommiwa@gmail.com>
2024-01-19 21:47:58 +0100

    Saving commits

--- src/screens/payments/components/Wallet.tsx
+++ src/screens/payments/components/Wallet.tsx
@@ -1,15 +1,20 @@
-import { Text } from '@/components/base'
-import { YStack } from 'tamagui'
+import { Button, Icon, Text } from '@/components/base'
+import { XStack, YStack } from 'tamagui'
 
 const Wallet = () => {
   return (
     <YStack px="$4">
-      <YStack p="$4" bg="white" gap="$4" borderRadius="$4">
+      <YStack p="$4" bg="white" gap="$3" borderRadius="$4">
         <Text type="h3">Wallet</Text>
-        <YStack gap="$1">
-          <Text type="body2">Available Balance</Text>
-          <Text type="h4">$1200</Text>
-        </YStack>
+        <XStack ai="center" jc="space-between">
+          <YStack>
+            <Text type="body2">Available Balance</Text>
+            <Text type="h4">$1200</Text>
+          </YStack>
+          <Button size="$5" circular>
+            <Icon name="Withdraw" />
+          </Button>
+        </XStack>
       </YStack>
     </YStack>
   )
