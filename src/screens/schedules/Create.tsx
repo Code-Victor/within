7b41ffabde437a64ca506fe80cc8ae2d1ae29e38
Lensatom <ayomidetommiwa@gmail.com>
2024-01-20 09:41:15 +0100

    Schedule for Victor to contyinue

--- src/screens/schedules/Create.tsx
+++ src/screens/schedules/Create.tsx
@@ -3,7 +3,8 @@ import { StackHeader } from '@/components/inc';
 import { useState } from 'react';
 import { ScrollView } from 'react-native';
 import { View, XStack, YStack } from 'tamagui';
-import DatePicker from "react-native-modern-datepicker";
+// import DatePicker from "react-native-modern-datepicker";
+import DateTimePicker from '@react-native-community/datetimepicker';
 
 const Create = () => {
 
@@ -13,6 +14,10 @@ const Create = () => {
 
   const closeModal = () => {
     setModalOpen(false)
+  }
+
+  const handleChange = (propDate:any) => {
+    console.log(propDate)
   }
 
   return (
@@ -36,20 +41,19 @@ const Create = () => {
               </XStack>
             </ScrollView>
           </View>
-          <YStack p="$4" mt="$4" gap="$6">
+          <YStack p="$4" mt="$4" gap="$8">
             <Input label="Title" />
-            {/* <Button type="ghost" onPress={() => setModalOpen(true)} asChild> */}
-              {/* <Input label="Date" disabled onPress={() => setModalOpen(true)} /> */}
-              <YStack gap="$1" onPress={() => setModalOpen(true)}>
-                <Text type="body2">Date</Text>
-                <View bg="white" h="$5" borderRadius="$4" borderWidth={1} borderColor={"$dark.2"}></View>
-              </YStack>
-            {/* </Button> */}
-            <Input label="Start Time" />
-            <Input label="End Time" />
+            <YStack gap="$1" onPress={() => setModalOpen(true)}>
+              <Text type="body2">Start Date</Text>
+              <View bg="white" h="$5" borderRadius="$4" borderWidth={1} borderColor={"$dark.2"}></View>
+            </YStack>
+            <YStack gap="$1" onPress={() => setModalOpen(true)}>
+              <Text type="body2">End Date</Text>
+              <View bg="white" h="$5" borderRadius="$4" borderWidth={1} borderColor={"$dark.2"}></View>
+            </YStack>
             <Modal open={modalOpen} onOpenChange={closeModal}>
               <Modal.Content f={1} w="100%" jc="center" px="$5" bg={"rgba(0, 0, 0, 0.1)"}>
-                <DatePicker />
+                <DateTimePicker value={new Date()} mode="time" />
                 <XStack w="100%" pt="$4" jc="center">
                   <Button onPress={closeModal} type="outline" circular size="$5" borderRadius={99} icon={<Icon name="Close" width={30} height={30} />} />
                 </XStack>
